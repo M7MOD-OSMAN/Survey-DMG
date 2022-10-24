9a0cd605ac213df1d5acd44e91ce6f1d00369559
M7MOD-OSMAN <M7MOD-OSMAN@github.com>
2022-10-24 11:10:23 +0200

    refactoring and separating components

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Questions from './Questions'
+import questions from '../questions.json'
+import Results from './Results'
 
 const Survey = () => {
+  const [data, setData] = useState(questions)
+
   return (
     <div className='survey-container'>
       <h1>The Parker Team Player Survey</h1>
-      <Questions />
+      <Questions data={data} setData={setData} />
+      <Results data={data} />
     </div>
   )
 }
